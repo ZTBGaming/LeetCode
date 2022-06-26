ed18d3bd5b9ef9ed7a7959064479023d45b2429e
Zach <104875665+ZachBlack@users.noreply.github.com>
2022-06-25 21:45:54 -0500

    Create symmetric_tree.py

---
+++ Binary Trees/symmetric_tree.py
@@ -0,0 +1,18 @@
+'''
+Symmetric Tree
+
+PROMPT: 
+        Given the root of a binary tree, check whether it is a mirror of itself
+        (i.e., symmetric around its center).
+        EXAMPLE TREE:
+            Input: root = [1,2,2,3,4,4,3]
+            Output: true
+                        1
+                    2   |   2
+                  3   4 | 4   3
+                        |
+        center line ->  |
+        
+        Returns: is it symmetric? (bool)
+'''
+
