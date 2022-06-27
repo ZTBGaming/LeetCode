e0b56d89d665741a3c405ec0301b7e586277522b
Zach <104875665+ZachBlack@users.noreply.github.com>
2022-06-26 20:52:30 -0500

    Update has_path_sum.py

--- Binary Trees/has_path_sum.py
+++ Binary Trees/has_path_sum.py
@@ -11,3 +11,28 @@ PROMPT:
         Returns: is targetSum possible? (bool)
 '''
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        
+        targetSum -= root.val
+        
+        # When no children exist, we know this is a leaf
+        if not root.left and not root.right:
+            return 0 == targetSum
+       
+        # Since there is more to this Tree, dive further while subtracting the node.val from targetSum
+        else:
+            left, right = False, False
+            if root.left:
+                left = self.hasPathSum(root.left, targetSum)
+            if root.right:
+                right = self.hasPathSum(root.right, targetSum)
+            return left or right
