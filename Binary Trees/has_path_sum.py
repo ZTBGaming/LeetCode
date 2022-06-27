d93ccc71f2ff7a6a239d05f078eddb54f205a10c
Zach <104875665+ZachBlack@users.noreply.github.com>
2022-06-26 19:29:15 -0500

    Create has_path_sum.py

---
+++ Binary Trees/has_path_sum.py
@@ -0,0 +1,13 @@
+
+'''
+Path Sum of a Binary Tree
+
+PROMPT: 
+        Given the root of a binary tree and an integer targetSum, return true if the tree
+        has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+        
+        A leaf is a node with no children.
+        
+        Returns: is targetSum possible? (bool)
+'''
+
