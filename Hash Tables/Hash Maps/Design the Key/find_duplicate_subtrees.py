20d1779b4deb021c3b8b2284de623e521f6b82d1
Zach <104875665+ZachBlack@users.noreply.github.com>
2022-07-03 17:39:33 -0500

    Update find_duplicate_subtrees.py

--- Hash Tables/Hash Maps/Design the Key/find_duplicate_subtrees.py
+++ Hash Tables/Hash Maps/Design the Key/find_duplicate_subtrees.py
@@ -3,7 +3,6 @@
             Given the root of a binary tree, return all duplicate subtrees.
 
             For each kind of duplicate subtrees, you only need to return the root node of any one of them.
-
             Two trees are duplicate if they have the same structure with the same node values.
 """
 
@@ -13,30 +12,28 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
-        duplicates = set()
-        tree_nodes = dict()
-    
-        def depth(root):
-            routing = []
-            if root:
-                routing.append("~"+str(root.val)+"#")
-                routing+=depth(root.left)
-                routing+=depth(root.right)
-                if routing:
-                    string = "".join(routing)
-            else:
-                string = 'null'
-            
-            if string in tree_nodes:
-                tree_nodes[string]+=1
-                if tree_nodes[string] == 2 and root:
-                    duplicates.add(root)
-            else:
-                tree_nodes[string] = 1
-                
-            return string
+        hashmap = dict()
+        duplicates = []
         
-        depth(root)
-        return duplicates
+        def preorder(root):
+            if root:
+                path = str(root.val)
+                path += "(" + preorder(root.left) + ")"
+                path += "(" + preorder(root.right) + ")"
+            else:
+                path = "NULL"
+            
+            if (path in hashmap) and (path != "NULL"):
+                if (hashmap[path] == 1):
+                    duplicates.append(root)
+                    hashmap[path] += 1
+            elif (path != "NULL"):
+                hashmap[path] = 1
+                
+            return path
+            
+        preorder(root)
+        return duplicates 
