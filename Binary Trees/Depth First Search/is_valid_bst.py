c0ed12bc72faf02e466124036ddcddeb5930b2d2
Zach <104875665+ZachBlack@users.noreply.github.com>
2022-07-08 00:43:37 -0500

    Create is_valid_bst.py

---
+++ Binary Trees/Depth First Search/is_valid_bst.py
@@ -0,0 +1,30 @@
+"""
+    PROMPT:
+            Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+            A valid BST is defined as follows:
+
+            The left subtree of a node contains only nodes with keys less than the node's key.
+            The right subtree of a node contains only nodes with keys greater than the node's key.
+            Both the left and right subtrees must also be binary search trees.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self.validate(root, -2 ** 32, 2 ** 32)
+        
+    def validate(self, root, lower_limit, upper_limit):
+        if root:
+            if not lower_limit < root.val < upper_limit:
+                return False
+            
+            return self.validate(root.left, lower_limit, root.val) and self.validate(root.right, root.val, upper_limit)
+            
+        else:
+            return True
