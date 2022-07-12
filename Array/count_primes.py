3a2db9b410ac11ce2e410c077d550a0cf124b79f
Zach <104875665+ZachBlack@users.noreply.github.com>
2022-07-11 20:12:50 -0500

    Update count_primes.py

--- Array/count_primes.py
+++ Array/count_primes.py
@@ -10,9 +10,15 @@ class Solution:
             return 0
         
         primes = [1] * n
+        # Initializing int's 0 and 1 as non-primes
         primes[0] = primes[1] = 0
+        
+        # Any number that is not prime will be divisible by a number less than, 
+        # or equal to it's square root and the paired number greater than,
+        # or equal to it's square root
         for i in range(2, int(n**0.5)+1):
             if primes[i]:
+                # If a number is prime then we know all multiples of it are NOT prime
                 for j in range(i*i, n, i):
                     primes[j] = 0
                 
